aab22a6de6520ddfd02f3b301963c672ea0602dd
Keaising <keaising@gmail.com>
2018-08-12 23:09:17 +0800

    finish

--- go/4.Median/median_test.go
+++ go/4.Median/median_test.go
@@ -3,8 +3,20 @@ package median
 import "testing"
 
 func TestMedian(t *testing.T) {
-	ret := findMedianSortedArrays([]int{2}, []int{1,3})
-	if ret != 2.0 {
-		t.Fatalf("expect:", ret)
+	tests := []struct{
+		a []int
+		b []int
+		expect float64
+	} {
+		{[]int{2}, []int{1,3},2},
+		{[]int{2,4}, []int{1, 5},3},
+		{[]int{2}, []int{5,6,7},5.5},
+	}
+
+	for _, test := range tests {
+		act:= findMedianSortedArrays(test.a, test.b)
+		if act != test.expect {
+			t.Errorf("%d %d expect: %f, actual: %f",test.a, test.b, test.expect, act)
+		}
 	}
 }
