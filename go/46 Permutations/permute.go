a3ffd045f4fb8a73d048f15d03379e5f58bba803
keaising <keaising@gmail.com>
2019-05-31 22:42:51 +0800

    commit problem 46 at 2019-05-31T22:42:51

--- go/46 Permutations/permute.go
+++ go/46 Permutations/permute.go
@@ -1,28 +1,47 @@
 package main
 
-func permute(nums []int) [][]int {
-	ret := [][]int{}
-	return ret
+import "fmt"
+
+func main() {
+	fmt.Println(permute([]int{1,0}))
 }
 
-func all(nums []int) [][]int {
-	if len(nums) == 0 {
-		return [][]int{}
-	}
+func permute(nums []int) [][]int {
+	result = [][]int{}
 	if len(nums) == 1 {
 		return [][]int{
 			nums,
 		}
 	}
+	array = &nums
+	generate(len(nums))
+	return result
+}
 
+var result [][]int
 
-	for i, n := range nums {
-		ret := all(append(nums[:i], nums[i+1:]...))
-		for j, r := range ret {
-			tmp := []int{}
-			tmp = copy(tmp, append(n))
+var array *[]int
+
+func generate(k int) {
+	if k == 1 {
+		ret := make([]int, len(*array))
+		copy(ret, *array)
+		result = append(result, ret)
+	} else {
+		generate(k - 1)
+		for i := 0; i < k-1; i++ {
+			if k%2 == 0 {
+				swap(i, k-1)
+			} else {
+				swap(0, k-1)
+			}
+			generate(k - 1)
 		}
 	}
 }
 
-
+func swap(i, j int) {
+	tem := (*array)[i]
+	(*array)[i] = (*array)[j]
+	(*array)[j] = tem
+}
