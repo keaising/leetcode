d70d6704b7d31f16cb1e36e962ba2074a82e5a41
keaising <keaising@gmail.com>
2019-06-03 00:14:47 +0800

    commit problem 67 at 2019-06-03T00:14:47

---
+++ go/67 Add Binary/add.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func addBinary(a string, b string) string {
+	length := 0
+	if len(a) > len(b) {
+		length = len(a)
+	} else {
+		length = len(b)
+	}
+	fmt.Println(length)
+	arr := make([]int32, length)
+	for i, r := range a {
+		arr[length-1-i] += r - '0'
+	}
+	fmt.Println(1, arr)
+
+	for i, r := range b {
+		arr[length-1-i] += r - '0'
+	}
+	fmt.Println(2, arr)
+	for i := length - 1; i > 0; i-- {
+		arr[i-1] += arr[i] % 2
+		arr[i] = arr[i] / 2
+	}
+	fmt.Println(3, arr)
+	result := ""
+	for i, v := range arr {
+		if i == 0 && v != 0 {
+			result = result + "1"
+		}
+		result = result + strconv.Itoa(int(v))
+	}
+	return result
+}
+
+func main() {
+	result := addBinary("11", "1")
+	fmt.Println(result)
+}
