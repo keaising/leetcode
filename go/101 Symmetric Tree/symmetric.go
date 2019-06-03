dc63dbed75325fae85ee185cb4261d5226f37a3b
keaising <keaising@gmail.com>
2019-06-03 23:17:31 +0800

    commit problem 101 at 2019-06-03T23:17:31

---
+++ go/101 Symmetric Tree/symmetric.go
@@ -0,0 +1,39 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return symmetric(root.Left, root.Right)
+}
+
+func symmetric(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil && right != nil {
+		return false
+	}
+	if left != nil && right == nil {
+		return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return symmetric(left.Left, right.Right) && symmetric(left.Right, right.Left)
+}
+
