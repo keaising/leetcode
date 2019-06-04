1b280d5d9264c052afdb1976004796a1c66de4e5
keaising <keaising@gmail.com>
2019-06-04 12:39:57 +0800

    commit at 2019-06-04T12:39:57

--- go/67 Add Binary/add.go
+++ go/67 Add Binary/add.go
@@ -13,19 +13,20 @@ func addBinary(a string, b string) string {
 		length = len(b)
 	}
 	fmt.Println(length)
-	arr := make([]int32, length)
+	arr := make([]int, length+1)
 	for i, r := range a {
-		arr[length-1-i] += r - '0'
+		arr[length-1-(len(a)-1-i)] += int(r - '0')
 	}
 	fmt.Println(1, arr)
 
 	for i, r := range b {
-		arr[length-1-i] += r - '0'
+		arr[length-1-i] += int(r - '0')
 	}
 	fmt.Println(2, arr)
 	for i := length - 1; i > 0; i-- {
 		arr[i-1] += arr[i] % 2
 		arr[i] = arr[i] / 2
+		fmt.Println(i, arr[i])
 	}
 	fmt.Println(3, arr)
 	result := ""
@@ -33,7 +34,7 @@ func addBinary(a string, b string) string {
 		if i == 0 && v != 0 {
 			result = result + "1"
 		}
-		result = result + strconv.Itoa(int(v))
+		result = result + strconv.Itoa(v)
 	}
 	return result
 }
