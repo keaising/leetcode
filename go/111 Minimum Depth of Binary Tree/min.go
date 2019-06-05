8cb8c14903db397624179088ee6b3a102ec67f83
keaising <keaising@gmail.com>
2019-06-05 13:04:28 +0800

    commit problem 111 at 2019-06-05T13:04:28

---
+++ go/111 Minimum Depth of Binary Tree/min.go
@@ -0,0 +1,27 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + less(minDepth(root.Left), minDepth(root.Right))
+}
+
+func less(a, b int) int {
+	if a == 0 {
+		return b
+	}
+	if b == 0 {
+		return a
+	}
+	if a > b {
+		return b
+	}
+	return a
+}
