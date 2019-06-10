40ea5a64840bd9c0a480acc9ead4fc8aa6d6a985
keaising <keaising@gmail.com>
2019-06-10 22:24:27 +0800

    commit at 2019-06-10T22:24:27

--- go/114 Flatten Binary Tree to Linked List/bst.go
+++ go/114 Flatten Binary Tree to Linked List/bst.go
@@ -7,38 +7,19 @@ type TreeNode struct {
 }
 
 func flatten(root *TreeNode) {
-	if root == nil {
-		return
-	}
-	q := Queue{}
-	q.fill(root)
 	curr := root
-	for _, node := range q {
-		curr.Left = nil
-		curr.Right = node
-		curr = node
+	for curr != nil {
+		if curr.Left != nil {
+			left := curr.Left
+			for left.Right != nil {
+				left = left.Right
+			}
+
+			left.Right = curr.Right
+			curr.Right = curr.Left
+			curr.Left = nil
+		}
+		curr = curr.Right
 	}
 }
 
-func (q Queue) fill(root *TreeNode) Queue {
-	if root != nil {
-		q = q.in(root)
-		q = q.fill(root.Left)
-		q = q.fill(root.Right)
-	}
-	return q
-}
-
-type Queue []*TreeNode
-
-func (q Queue) in(node *TreeNode) Queue {
-	q = append(q, node)
-	return q
-}
-
-func (q Queue) pop() (*TreeNode, Queue) {
-	if len(q) == 0 {
-		return nil, q
-	}
-	return q[0], q[1:]
-}
