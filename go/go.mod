b12adf8f722943a0cc78481f63162b7336a0e0fa
keaising <keaising@gmail.com>
2019-06-10 14:16:42 +0800

    add module

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/keaising/leetcode/golang
+
+go 1.12
---
+++ go/main.go
@@ -0,0 +1,15 @@
+package golang
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+
