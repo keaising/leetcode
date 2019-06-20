a38fa22255a3b561cbe10fc086014dcc39dbdffc
keaising <keaising@gmail.com>
2019-06-20 13:04:08 +0800

    commit problem 83 at 2019-06-20T13:04:07

---
+++ go/83 Remove Duplicates from Sorted List/delete.go
@@ -0,0 +1,13 @@
+package main
+
+func deleteDuplicates2(head *ListNode) *ListNode {
+	p := head
+	for p != nil && p.Next != nil {
+		if p.Val == p.Next.Val {
+			p.Next = p.Next.Next
+		} else {
+			p = p.Next
+		}
+	}
+	return head
+}
