bdb39c4e2966883c3e6f2fe7ab0d283152b260d8
keaising <keaising@gmail.com>
2019-06-26 10:56:10 +0800

    commit problem 155 at 2019-06-26T10:56:10

---
+++ 155.min-stack.go
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=155 lang=golang
+ *
+ * [155] Min Stack
+ */
+type MinStack struct {
+	stack []int
+	min   []int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{
+		stack: []int{},
+		min:   []int{},
+	}
+}
+
+func (this *MinStack) Push(x int) {
+	if len(this.stack) == 0 {
+		this.min = append(this.min, x)
+	} else {
+		this.min = append(this.min, smaller(this.min[len(this.min)-1], x))
+	}
+	this.stack = append(this.stack, x)
+}
+
+func (this *MinStack) Pop() {
+	if len(this.stack) > 0 {
+		this.stack = this.stack[:len(this.stack)-1]
+		this.min = this.min[:len(this.min)-1]
+	}
+}
+
+func (this *MinStack) Top() int {
+	if len(this.stack) > 0 {
+		return this.stack[len(this.stack)-1]
+	}
+	return 0
+}
+
+func (this *MinStack) GetMin() int {
+	if len(this.min) > 0 {
+		return this.min[len(this.min)-1]
+	}
+	return 0
+}
+
+func smaller(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
---
+++ go/155 Min Stack/stack.go
@@ -0,0 +1,51 @@
+package main
+
+type MinStack struct {
+	stack []int
+	min   []int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{
+		stack: []int{},
+		min:   []int{},
+	}
+}
+
+func (this *MinStack) Push(x int) {
+	if len(this.stack) == 0 {
+		this.min = append(this.min, x)
+	} else {
+		this.min = append(this.min, smaller(this.min[len(this.min)-1], x))
+	}
+	this.stack = append(this.stack, x)
+}
+
+func (this *MinStack) Pop() {
+	if len(this.stack) > 0 {
+		this.stack = this.stack[:len(this.stack)-1]
+		this.min = this.min[:len(this.min)-1]
+	}
+}
+
+func (this *MinStack) Top() int {
+	if len(this.stack) > 0 {
+		return this.stack[len(this.stack)-1]
+	}
+	return 0
+}
+
+func (this *MinStack) GetMin() int {
+	if len(this.min) > 0 {
+		return this.min[len(this.min)-1]
+	}
+	return 0
+}
+
+func smaller(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
