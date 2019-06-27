8b60cdbccf8b1934863892a4ddad664ad1b47c56
keaising <keaising@gmail.com>
2019-06-27 21:28:10 +0800

    commit problem 213 at 2019-06-27T21:28:09

---
+++ go/213 House Robber/rob.go
@@ -0,0 +1,44 @@
+package main
+
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	return bigger(helper(append([]int{}, nums[:len(nums)-1]...)), helper(append([]int{}, nums[1:]...)))
+}
+
+func helper(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if len(nums) == 2 {
+		return bigger(nums[0], nums[1])
+	}
+	if len(nums) == 3 {
+		return bigger(nums[0], nums[1], nums[0]+nums[2])
+	}
+	nums[2] = nums[0] + nums[2]
+	for i := 3; i < len(nums); i++ {
+		nums[i] = bigger(nums[i]+nums[i-2], nums[i]+nums[i-3])
+	}
+	return bigger(nums[len(nums)-1], nums[len(nums)-2])
+}
+
+func bigger(nums ...int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	ret := nums[0]
+	for _, n := range nums {
+		if n > ret {
+			ret = n
+		}
+	}
+	return ret
+}
