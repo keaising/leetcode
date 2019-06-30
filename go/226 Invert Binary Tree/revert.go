44fb9d47f18f9df79ffe3b72724237e997be9e38
keaising <keaising@gmail.com>
2019-06-30 19:41:04 +0800

    commit problem 226 at 2019-06-30T19:41:04

--- go/226 Invert Binary Tree/revert.go
+++ go/226 Invert Binary Tree/revert.go
@@ -15,3 +15,24 @@ func invertTree(root *TreeNode) *TreeNode {
 	invertTree(root.Right)
 	return root
 }
+
+
+func invertTree2(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	queue := [](*TreeNode){}
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+		curr.Left, curr.Right = curr.Right, curr.Left
+		if curr.Left != nil {
+			queue = append(queue, curr.Left)
+		}
+		if curr.Right != nil {
+			queue = append(queue, curr.Right)
+		}
+	}
+	return root
+}
