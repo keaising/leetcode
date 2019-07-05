8506a0758980aa7c4f510dd7516308146b7de58e
keaising <keaising@gmail.com>
2019-07-05 21:29:13 +0800

    commit problem 234 at 2019-07-05T21:29:13

--- go/234 Palindrome Linked List/isPalindrome.go
+++ go/234 Palindrome Linked List/isPalindrome.go
@@ -25,3 +25,33 @@ func isPalindrome(head *ListNode) bool {
 	}
 	return true
 }
+
+func isPalindrome2(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+	p1, p2 := head, head
+	for p2 != nil && p2.Next != nil {
+		p1 = p1.Next
+		p2 = p2.Next.Next
+	}
+	if p2 != nil {
+		p1 = p1.Next
+	}
+
+	var prev *ListNode
+	for p1 != nil {
+		next := p1.Next
+		p1.Next = prev
+		prev = p1
+		p1 = next
+	}
+	for prev != nil && head != nil {
+		if prev.Val != head.Val {
+			return false
+		}
+		prev = prev.Next
+		head = head.Next
+	}
+	return true
+}
