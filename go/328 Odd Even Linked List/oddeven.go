bc82b1aff0d83c8c5ec97c2cfe718e984f01dd8c
keaising <keaising@gmail.com>
2019-07-07 10:20:49 +0800

    commit problem 328 at 2019-07-07T10:20:49

--- go/328 Odd Even Linked List/oddeven.go
+++ go/328 Odd Even Linked List/oddeven.go
@@ -6,5 +6,24 @@ type ListNode struct {
 }
 
 func oddEvenList(head *ListNode) *ListNode {
-
+	if head == nil || head.Next == nil {
+		return head
+	}
+	p1, p2 := head, head.Next
+	cur := head.Next.Next
+	n1, n2 := p1, p2
+	for i := 0; cur != nil; i++ {
+		if i%2 == 0 {
+			n1.Next = cur
+			n1 = n1.Next
+		} else {
+			n2.Next = cur
+			n2 = n2.Next
+		}
+		cur = cur.Next
+	}
+	n1.Next = p2
+	n2.Next = nil
+	
+	return p1
 }
---
+++ go/328 Odd Even Linked List/oddeven_test.go
@@ -0,0 +1,67 @@
+package main
+
+import "testing"
+
+func TestOddEven(t *testing.T) {
+	tests := []struct {
+		nums []int
+		ans  []int
+	}{
+		{
+			nums: []int{1, 2, 3, 4, 5, 6, 7},
+			ans:  []int{1, 3, 2, 6, 3, 5, 4},
+		},
+	}
+	for _, test := range tests {
+		list := buildListNode(test.nums)
+		list = oddEvenList(list)
+		nums := buildNums(list)
+		if sliceEqual(test.ans, nums) {
+			t.Error("!!!!")
+		}
+	}
+}
+
+func buildListNode(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	head := &ListNode{
+		Val:  nums[0],
+		Next: nil,
+	}
+	node := head
+	for i := 1; i < len(nums); i++ {
+		node.Next = &ListNode{
+			Val:  nums[i],
+			Next: nil,
+		}
+		node = node.Next
+	}
+	return head
+}
+
+func buildNums(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
+	ans := []int{}
+	for head != nil {
+		ans = append(ans, head.Val)
+		head = head.Next
+	}
+	return ans
+}
+
+func sliceEqual(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
