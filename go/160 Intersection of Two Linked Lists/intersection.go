d207cb343dcd7cb2178bf954936959d0aba4e3a6
keaising <keaising@gmail.com>
2019-07-07 17:07:51 +0800

    commit problem 160 at 2019-07-07T17:07:51

---
+++ go/160 Intersection of Two Linked Lists/intersection.go
@@ -0,0 +1,27 @@
+package main
+
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	l1, l2 := headA, headB
+	for l1 != nil || l2 != nil {
+		if l1 == l2 {
+			return l1
+		}
+		if l1 == nil {
+			l1 = headB
+		} else {
+			l1 = l1.Next
+		}
+		if l2 == nil {
+			l2 = headA
+		} else {
+			l2 = l2.Next
+		}
+	}
+	return nil
+}
