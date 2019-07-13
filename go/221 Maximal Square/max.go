869c8af8c67842782467a2602456f1d543b8e6de
keaising <keaising@gmail.com>
2019-07-13 23:47:50 +0800

    commit problem 221 at 2019-07-13T23:47:49

--- go/221 Maximal Square/max.go
+++ go/221 Maximal Square/max.go
@@ -1,1 +1,53 @@
-package _21_Maximal_Square
+package main
+
+func maximalSquare(matrix [][]byte) int {
+	if matrix == nil || len(matrix) == 0 {
+		return 0
+	}
+	m, n := len(matrix), len(matrix[0])
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+	ans := 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 || matrix[i][j] == '0' {
+				dp[i][j] = int(matrix[i][j] - '0')
+			} else {
+				dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + int(matrix[i][j]-'0')
+			}
+			ans = bigger(ans, dp[i][j])
+		}
+	}
+	return ans * ans
+}
+
+func min(a, b, c int) int {
+	switch {
+	case a >= b || a >= c:
+		return smaller(b, c)
+	case b >= c || b >= a:
+		return smaller(a, c)
+	case c >= a || c >= b:
+		return smaller(a, b)
+	default:
+		return 0
+	}
+}
+
+func smaller(a, b int) int {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func bigger(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
--- go/221 Maximal Square/max_test.go
+++ go/221 Maximal Square/max_test.go
@@ -1,1 +1,32 @@
 package main
+
+import "testing"
+
+func TestMax(t *testing.T) {
+	testCases := []struct {
+		name   string
+		input  [][]byte
+		expect int
+	}{
+		{
+			name: "first",
+			input: [][]byte{
+				{'1', '0', '1', '1', '0', '1'},
+				{'1', '1', '1', '1', '1', '1'},
+				{'0', '1', '1', '0', '1', '1'},
+				{'1', '1', '1', '0', '1', '0'},
+				{'0', '1', '1', '1', '1', '1'},
+				{'1', '1', '0', '1', '1', '1'},
+			},
+			expect: 4,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expect != maximalSquare(tc.input) {
+				t.Error("!!!!")
+			}
+		})
+	}
+}
