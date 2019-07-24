17e20574be718483d9de3e92610d4e1ed13bf07f
keaising <keaising@gmail.com>
2019-07-24 23:32:44 +0800

    commit problem 60 at 2019-07-24T23:32:44

--- go/60 Permutation Sequence/permutation.go
+++ go/60 Permutation Sequence/permutation.go
@@ -1,41 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
-
-func main() {
-	fmt.Println(getPermutation(4, 6))
-}
 
 func getPermutation(n int, k int) string {
 	list := []string{}
 	for i := 1; i <= n; i++ {
 		list = append(list, strconv.Itoa(i))
 	}
-	ans := []string{}
-	for len(list) > 0 {
-		f := fact(len(list) - 1)
+	k--
+	fact := []int{1}
+	for i := 1; i <= n; i++ {
+		fact = append(fact, fact[len(fact)-1]*i)
+	}
+	var ans []string
+	for i := n; i >= 1; i-- {
+		f := fact[i-1]
 		y := k / f
 		k = k % f
-		fmt.Println("y", y, "k", k, "len", len(list))
 		ans = append(ans, list[y])
 		list = remove(list, y)
 	}
-	return strings.Join(ans, ",")
+	return strings.Join(ans, "")
 }
 
 func remove(s []string, i int) []string {
 	return append(s[:i], s[i+1:]...)
-}
-
-func fact(n int) int {
-	fmt.Println("n:", n)
-	if n >= 1 {
-		return 1
-	} else {
-		return n * fact(n-1)
-	}
 }
