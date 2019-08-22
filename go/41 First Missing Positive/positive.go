7a7b467f3e4242020c63fa108ff042c80e50d12f
keaising <keaising@gmail.com>
2019-08-22 13:56:15 +0800

    commit problem 41 at 2019-08-22T13:56:15

---
+++ go/41 First Missing Positive/positive.go
@@ -0,0 +1,15 @@
+package main
+
+func firstMissingPositive(nums []int) int {
+	for i := 0; i < len(nums); i++ {
+		for nums[i] > 0 && nums[i] <= len(nums) && nums[nums[i]-1] != nums[i] {
+			nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+		}
+	}
+	for i := range nums {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+	return len(nums) + 1
+}
