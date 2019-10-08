8ba6b4de752a08c9fcc3ab15d9f7083ee4beb1b8
keaising <keaising@gmail.com>
2019-10-08 22:13:51 +0800

    2. Add Two Numbers

--- python/2. Add Two Numbers/add.py
+++ python/2. Add Two Numbers/add.py
@@ -4,18 +4,19 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         head, curr = ListNode(0)
         carry = 0
         while l1 or l2 or carry:
             if l1:
-                carry+=l1.val
+                carry += l1.val
                 l1 = l1.next
             if l2:
-                carry+=l2.val
+                carry += l2.val
                 l2 = l2.next
-            curr.next = ListNode(carry%10)
-            cur = cur.next
+            curr.next = ListNode(carry % 10)
+            curr = curr.next
             carry //= 10
         return head.next
